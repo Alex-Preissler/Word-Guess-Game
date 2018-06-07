7770418c50b812b2afc4f15e30eba4ee79b97965
Alex-Preissler <AlexJPreissler@Outlook.com>
2018-06-07 16:30:58 -0400

    semi-final copy, will add music later

--- Resources/javascript/game.js
+++ Resources/javascript/game.js
@@ -1,18 +1,47 @@
-var bands=[
-    "Blink 182", 
-    "Green Day", 
-    "Rage Against The Machine", 
-    "Foo Fighters", 
-    "Eve 6", 
-    "311", 
-    "Red Hot Chili Peppers", 
-    "The Offspring", 
-    "Nine Inch Nails", 
-    "Weezer", 
-    "Bush", 
-    "Filter", 
-    "Stone Temple Pilots", 
-    "Nirvana"];
+var bands={
+    
+    1: {name: "Blink 182", 
+        img: "Resources/images/blink-182.jpg"}, 
+    
+    2: {name: "Green Day", 
+        img: "Resources/images/greenday.jpg"},
+   
+    3: {name: "Rage Against The Machine", 
+        img: "Resources/images/rage-against-the-machine.jpg"},
+    
+    4: {name: "Foo Fighters", 
+        img: "Resources/images/foo-fighters.jpg"},
+   
+    5: {name: "Eve 6", 
+        img: "Resources/images/eve-6.jpg"},
+    
+    6: {name: "311", 
+        img: "Resources/images/311.jpg"},
+    
+    7: {name: "Red Hot Chili Peppers", 
+        img: "Resources/images/red-hot-chili-peppers.jpg"},
+   
+    8: {name: "The Offspring", 
+        img: "Resources/images/the-offspring.jpg"},
+    
+    9: {name: "Nine Inch Nails", 
+        img: "Resources/images/nine-inch-nails.jpg"},
+    
+    10: {name: "Weezer", 
+        img: "Resources/images/weezer.jpg"} ,
+    
+    11: {name: "Bush", 
+        img: "Resources/images/bush.jpg"},
+    
+    12: {name: "Filter", 
+        img: "Resources/images/filter.jpg"},
+    
+    13: {name: "Stone Temple Pilots", 
+        img: "Resources/images/stone-temple-pilots.jpg"}, 
+    
+    14: {name: "Nirvana", 
+        img: "Resources/images/nirvana.jpg"}
+};
 var wins = 0;
 var word;
 var guess;
@@ -22,15 +51,21 @@ var ltrCorrect = 0;
 var trigger = 0;
 var prevent = [];
 var match = false;
-var imgIndex = 0;
+var band;
+var imgSrc;
+
 
 //Selects word from array
 function selectWord() {
 
-    word = bands[Math.floor(Math.random() * bands.length)]
-    imgIndex = jQuery.inArray(word, bands);
-    console.log("Index" + imgIndex);
+    band = bands[Math.floor((Math.random() * 14)) + 1]
+
+    word = band.name;
+
+    imgSrc = band.img;
+
     console.log(word);
+    console.log(imgSrc);
     console.log("-----------------------------------------------");
     
 
@@ -40,7 +75,7 @@ function selectWord() {
 function updateScore() {
 
     $("#wins").text("Wins: " + wins);
-
+    
 }
 
 //Displays Correct number of blank spaces and breaks in word
@@ -100,7 +135,8 @@ function check() {
        
         lwrCase = lwrCase.toLowerCase();
         
-       
+        console.log(guess);
+        console.log(lwrCase);
        
         if(lwrCase == guess){
 
@@ -191,13 +227,59 @@ function preventDouble(){
 
 function AssignImg() {
 
-    $("#imgTarget").
+    $("#imgTarget").attr("src", imgSrc);
+    console.log(imgSrc);
+
+}
+
+function displayImg(){
+
+    $("#imgTarget").css("visibility", "visible");
+    console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+}
+
+function hideImg() {
+
+    $("#imgTarget").css("visibility", "hidden");
+
+}
+
+function winAlert(callback) {
+
+    var alertMonitor = false;
+    
+    while(alertMonitor == false){
+        
+        alert("Congratulations! You Win!");
+        alertMonitor = true;
+        console.log("bob");
+    }
+   
+    alertMonitor = false;
+    callback();
+}
+
+function roundReset(){
+
+    console.log("fred");
+    hideImg();        
+    resetVar();
+    clearLetterList();
+    guessUpdateReset();
+    selectWord();
+    AssignImg();
+    displayBlanks();
+    ltrIdAssign();
+    main();
+
 
 }
 
 $(document).ready(function(){
 
     selectWord();
+    AssignImg();
     updateScore();
     displayBlanks();
     ltrIdAssign();
@@ -226,16 +308,22 @@ function main(){
     
         }else if(ltrCount == ltrCorrect){
             
+          
             wins++;
             updateScore();
-            alert("Congratulations! You Win!");
-            resetVar();
-            clearLetterList();
-            guessUpdateReset();
-            selectWord();
-            displayBlanks();
-            ltrIdAssign();
-            main();
+            
+            displayImg();
+            
+            
+            jQuery(function($) {    
+                
+                $.when($("#imgTarget").html('')).done(function() {
+                    
+                    winAlert(roundReset);
+                
+                });
+            });
+         
         
         }else{
             
